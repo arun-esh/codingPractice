1fe71df196805f0c80087dd492ec3cd4c985560a
arunesh <nysdas@icloud.com>
2022-06-18 00:06:49 -0400

    add Node Class
    There were two different files Node and myLinkedList. I combined them into one.
    Created two methods for LinkedList
    - push
    - print
    
    PUSH to add data to the end and PRINT to display the list.

--- myLinkedList/myLinkedList.java
+++ myLinkedList/myLinkedList.java
@@ -1,22 +1,49 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node() {
+        this.data = 0;
+    }
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
 class myLinkedList {
-  private Node head;
-  private Node tail;
-  private int length;
+    private Node head;
+    private Node tail;
+    private int length;
 
-  public myLinkedList () {
-    this.length = 0;
-    this.head = null;
-    this.tail = null;
-  }
+    public myLinkedList() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
 
-  // push the element to the LinkedList
-  public void push (int data) {
-    Node newNode = new Node(data);
-    if(this.head == null){
-      head = newNode;  
+    // push the element to the LinkedList
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (this.head == null) {
+            head = newNode;
+        } else {
+            this.tail.next = newNode;
+        }
+
+        length++;
+        tail = newNode;
     }
-    else {
-      this.tail.next = newNode;
+
+    // print the LinkedList
+
+    public void printList() {
+        Node dummyNode = head;
+        for (int i = 0; i < length; i++) {
+            if (dummyNode != null) {
+                System.out.print(dummyNode.data + " ");
+            }
+            System.out.println();
+        }
     }
-  }
 }
