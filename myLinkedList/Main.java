02c2a95bb6d0696f05f15db4450145efe4c6fbdb
Arunesh Choudhary <nysdas@icloud.com>
2022-06-22 15:18:20 -0400

    add HackerRank insert-a-node-at-the-tail-of-a-linked-list (#14)
    Hacker Rank Link
    https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
    
    Head and data was provided as the paramters.
    Had to return the head

--- myLinkedList/Main.java
+++ myLinkedList/Main.java
@@ -3,14 +3,20 @@ public class Main {
 
         myLinkedList myList = new myLinkedList();
 
-        myList.push(34);
-
-        myList.push(64);
+        
         myList.push(84);
         myList.push(94);
 
         myList.printList();
+
+        myList.addToFront(55);
+        myList.push(34);
+        myList.push(64);
         
+        myList.printList();
+
+        myList.removeFromFront();
+        myList.printList();
     }
     
 }
--- myLinkedList/myLinkedList.java
+++ myLinkedList/myLinkedList.java
@@ -1,49 +1,83 @@
-class Node {
-    int data;
-    Node next;
-
-    public Node() {
-        this.data = 0;
-    }
-
-    public Node(int data) {
-        this.data = data;
-    }
-}
-
 class myLinkedList {
     private Node head;
-    private Node tail;
     private int length;
 
     public myLinkedList() {
         this.length = 0;
-        // this.head = null;
-        // this.tail = null;
+    }
+
+
+    // Getter and Setter for Length Node
+    public int getSize(){
+        return length;
     }
 
     // push the element to the LinkedList
     public void push(int data) {
         Node newNode = new Node(data);
-        if (this.head == null) {
+        Node currentNode = head;
+        if (isEmpty()) {
             head = newNode;
         } else {
-            this.tail.next = newNode;
+            while(currentNode.getNext() != null){
+                System.out.println("length: " + length);
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(newNode);
         }
 
         length++;
-        tail = newNode;
+        System.out.println("length: " + length);
+    }
+
+    // Add Element to the front of the list
+    public void addToFront(int data){
+        Node newNode = new Node(data);
+        length++;
+        if(!isEmpty()){
+            newNode.setNext(head);
+        }
+        head = newNode;
+        System.out.println("length: " + length);
+    }
+
+    // Remove from the front
+
+    public void removeFromFront(){
+        if(isEmpty()){
+            System.out.println("Empty List.");
+        }
+        else{
+            if(getSize() == 1){
+                head = null;
+            }else{
+                head = head.getNext();
+            }
+        }
+        length--;
+    }
+
+    // Check if Empty
+
+    public Boolean isEmpty(){
+        if(length == 0) return true;
+        return false;
     }
 
     // print the LinkedList
 
     public void printList() {
-        Node currentNode = head;
-        for (int i = 0; i < length; i++) {
-            if (currentNode != null) {
-                System.out.print(currentNode.data + " ");
+        if(isEmpty()){
+            System.out.println("Empty List");
+        }else{
+            Node currentNode = head;
+            System.out.print("HEAD:-> ");
+            for (int i = 0; i < length; i++) {
+                if (currentNode != null) {
+                    System.out.print(currentNode.getData() + " ");
+                }
+                currentNode = currentNode.getNext();
             }
-            currentNode = currentNode.next;
             System.out.println();
         }
     }
